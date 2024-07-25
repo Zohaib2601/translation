8f8392bc70f684156a8b93b3e44054e79ed83550
Zohaib2601 <102287801+Zohaib2601@users.noreply.github.com>
2024-07-25 13:10:55 +0530

    Add files via upload

---
+++ countries.js
@@ -0,0 +1,197 @@
+const countries = [
+    
+   { c1: "am-ET", n1: "Amharic",},
+
+   { c1: "ar-SA", n1: "Arabic",},
+
+   { c1: "be-BY", n1: "Bielarus",},
+
+    {c1: "bem-ZM", n1: "Bemba",},
+
+   { c1: "bi-VU", n1: "Bislama",},
+
+    {c1: "bjs-BB", n1: "Bajan",},
+
+   { c1: "bn-IN", n1: "Bengali",},
+
+   { c1: "bo-CN", n1: "Tibetan",},
+
+   { c1: "br-FR", n1: "Breton",},
+
+   { c1: "bs-BA", n1: "Bosnian",},
+
+   { c1: "ca-ES", n1: "Catalan",},
+
+    {c1: "cop-EG", n1: "Coptic",},
+
+   { c1: "cs-CZ", n1: "Czech",},
+
+   { c1: "cy-GB", n1: "Welsh",},
+
+   { c1: "da-DK", n1: "Danish",},
+
+   { c1: "dz-BT", n1: "Dzongkha",},
+
+   { c1: "de-DE", n1: "German",},
+
+   { c1: "dv-MV", n1: "Maldivian",},
+
+   { c1: "el-GR", n1: "Greek",},
+
+   { c1: "en-GB", n1: "English",},
+
+   { c1: "es-ES", n1: "Spanish",},
+
+   { c1: "et-EE", n1: "Estonian",},
+
+   { c1: "eu-ES", n1: "Basque",},
+
+   { c1: "fa-IR", n1: "Persian",},
+
+   { c1: "fi-FI", n1: "Finnish",},
+
+    {c1: "fn-FNG", n1: "Fanagalo",},
+
+   { c1: "fo-FO", n1: "Faroese",},
+
+   { c1: "fr-FR", n1: "French",},
+
+   { c1: "gl-ES", n1: "Galician",},
+
+   { c1: "gu-IN", n1: "Gujarati",},
+
+   { c1: "ha-NE", n1: "Hausa",},
+
+   { c1: "he-IL", n1: "Hebrew",},
+
+   { c1: "hi-IN", n1: "Hindi",},
+
+   { c1: "hr-HR", n1: "Croatian",},
+
+   { c1: "hu-HU", n1: "Hungarian",},
+
+   { c1: "id-ID", n1: "Indonesian",},
+
+   { c1: "is-IS", n1: "Icelandic",},
+
+   { c1: "it-IT", n1: "Italian",},
+
+   { c1: "ja-JP", n1: "Japanese",},
+
+   { c1: "kk-KZ", n1: "Kazakh",},
+
+   { c1: "km-KM", n1: "Khmer",},
+
+   { c1: "kn-IN", n1: "Kannada",},
+
+   { c1: "ko-KR", n1: "Korean",},
+
+   { c1: "ku-TR", n1: "Kurdish",},
+
+   { c1: "ky-KG", n1: "Kyrgyz",},
+
+   { c1: "la-VA", n1: "Latin",},
+
+   { c1: "lo-LA", n1: "Lao",},
+
+   { c1: "lv-LV", n1: "Latvian",},
+
+    {c1: "men-SL", n1: "Mende",},
+
+   { c1: "mg-MG", n1: "Malagasy",},
+
+   { c1: "mi-NZ", n1: "Maori",},
+
+   { c1: "ms-MY", n1: "Malay",},
+
+   { c1: "mt-MT", n1: "Maltese",},
+
+   { c1: "my-MM", n1: "Burmese",},
+
+   { c1: "ne-NP", n1: "Nepali",},
+
+    {c1: "niu-NU", n1: "Niuean",},
+
+   { c1: "nl-NL", n1: "Dutch",},
+
+   { c1: "no-NO", n1: "Norwegian",},
+
+   { c1: "ny-MW", n1: "Nyanja",},
+
+   { c1: "ur-PK", n1: "Pakistani",},
+
+    {c1: "pau-PW", n1: "Palauan",},
+
+   { c1: "pa-IN", n1: "Panjabi",},
+
+   { c1: "ps-PK", n1: "Pashto",},
+
+    {c1: "pis-SB", n1: "Pijin",},
+
+   { c1: "pl-PL", n1: "Polish",},
+
+   { c1: "pt-PT", n1: "Portuguese",},
+
+   { c1: "rn-BI", n1: "Kirundi",},
+
+   { c1: "ro-RO", n1: "Romanian",},
+
+   { c1: "ru-RU", n1: "Russian",},
+
+   { c1: "sg-CF", n1: "Sango",},
+
+   { c1: "si-LK", n1: "Sinhala",},
+
+   { c1: "sk-SK", n1: "Slovak",},
+
+   { c1: "sm-WS", n1: "Samoan",},
+
+   { c1: "sn-ZW", n1: "Shona",},
+
+   { c1: "so-SO", n1: "Somali",},
+
+   { c1: "sq-AL", n1: "Albanian",},
+
+   { c1: "sr-RS", n1: "Serbian",},
+
+   { c1: "sv-SE", n1: "Swedish",},
+
+   { c1: "sw-SZ", n1: "Swahili",},
+
+   { c1: "ta-LK", n1: "Tamil",},
+
+   { c1: "te-IN", n1: "Telugu",},
+
+    {c1: "tet-TL", n1: "Tetum",},
+
+   { c1: "tg-TJ", n1: "Tajik",},
+
+   { c1: "th-TH", n1: "Thai",},
+
+   { c1: "ti-TI", n1: "Tigrinya",},
+
+   { c1: "tk-TM", n1: "Turkmen",},
+
+   { c1: "tl-PH", n1: "Tagalog",},
+
+   { c1: "tn-BW", n1: "Tswana",},
+
+   { c1: "to-TO", n1: "Tongan",},
+
+   { c1: "tr-TR", n1: "Turkish",},
+
+   { c1: "uk-UA", n1: "Ukrainian",},
+
+   { c1: "uz-UZ", n1: "Uzbek",},
+
+   { c1: "vi-VN", n1: "Vietnamese",},
+
+   { c1: "wo-SN", n1: "Wolof",},
+
+   { c1: "xh-ZA", n1: "Xhosa",},
+
+   { c1: "yi-YD", n1: "Yiddish",},
+
+   { c1: "zu-ZA", n1: "Zulu"},
+
+]
