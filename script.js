a71f4e04884c4239708f68821f76883d8fce625c
Zohaib2601 <102287801+Zohaib2601@users.noreply.github.com>
2024-07-28 09:50:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -134,12 +134,6 @@ downloadBtn.addEventListener("click", (e) => {
   }
 });
 
-const darkModeCheckbox = document.getElementById("dark-mode-btn");
-
-darkModeCheckbox.addEventListener("change", () => {
-  document.body.classList.toggle("dark");
-});
-
 const inputChars = document.querySelector("#input-chars");
 
 inputTextElem.addEventListener("input", (e) => {
